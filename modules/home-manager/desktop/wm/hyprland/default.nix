ae9f4a8400edcaeaff1e77d0688f69c037d1c8ef
ya <gitogitohub@gmail.com>
2025-12-20 17:16:13 +0900

    hyprland config optimize

--- modules/home-manager/desktop/wm/hyprland/default.nix
+++ modules/home-manager/desktop/wm/hyprland/default.nix
@@ -1,33 +1,66 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
+
+let
+  terminal = "${pkgs.alacritty}/bin/alacritty";
+  menu = "${pkgs.fuzzel}/bin/fuzzel";
+  fileManager = "${pkgs.xfce.thunar}/bin/thunar";
+  lock = "${pkgs.swaylock-effects}/bin/swaylock";
+  logout = "${pkgs.wlogout}/bin/wlogout";
+
+  # Tools
+  jq = "${pkgs.jq}/bin/jq";
+  grim = "${pkgs.grim}/bin/grim";
+  slurp = "${pkgs.slurp}/bin/slurp";
+  wlCopy = "${pkgs.wl-clipboard}/bin/wl-copy";
+  cliphist = "${pkgs.cliphist}/bin/cliphist";
+  zenity = "${pkgs.zenity}/bin/zenity";
+
+  # Audio / Hardware
+  wpctl = "${pkgs.wireplumber}/bin/wpctl";
+  brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+
+  # Screenshot
+  screenshotActive = pkgs.writeShellScript "screenshot-active" ''
+    FILEPATH=$(${zenity} --file-selection --save --confirm-overwrite --filename="$HOME/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S_active.png)")
+    if [ -n "$FILEPATH" ]; then
+      ${grim} -g "$(${pkgs.hyprland}/bin/hyprctl activewindow -j | ${jq} -r "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])")" "$FILEPATH"
+    fi
+  '';
+  screenshotAll = pkgs.writeShellScript "screenshot-all" ''
+    FILEPATH=$(${zenity} --file-selection --save --confirm-overwrite --filename="$HOME/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S_all.png)")
+    if [ -n "$FILEPATH" ]; then
+      ${grim} "$FILEPATH"
+    fi
+  '';
+
+in
 
 {
   # packages
-  home.packages = with pkgs; [
-    alacritty
-    fuzzel
-    waybar
-    dunst
-    libnotify
-    networkmanagerapplet
-    wlogout
-    hyprpaper
-    xfce.thunar
-    xfce.thunar-volman
-    xfce.thunar-archive-plugin
-    blueman
-    wayvnc
+  home.packages = [
+    pkgs.alacritty
+    pkgs.fuzzel
+    pkgs.waybar
+    pkgs.dunst
+    pkgs.networkmanagerapplet
+    pkgs.wlogout
+    pkgs.hyprpaper
+    pkgs.xfce.thunar
+    pkgs.xfce.thunar-volman
+    pkgs.xfce.thunar-archive-plugin
+    pkgs.blueman
+    pkgs.wayvnc
 
-    wl-clipboard
-    cliphist
-    grim
-    slurp
-    swaylock-effects
-    jq
-    zenity
+    pkgs.wl-clipboard
+    pkgs.cliphist
+    pkgs.grim
+    pkgs.slurp
+    pkgs.swaylock-effects
+    pkgs.jq
+    pkgs.zenity
 
-    playerctl
-    brightnessctl
-    wireplumber
+    pkgs.brightnessctl
+    pkgs.wireplumber
   ];
 
   # Hyprland
@@ -36,21 +69,20 @@
 
     settings = {
       "$mainMod" = "SUPER";
-      "$terminal" = "alacritty";
-      "$menu" = "fuzzel --hide-before-typing --lines 3 --width 40";
+      "$terminal" = terminal;
+      "$menu" = "${menu} --hide-before-typing --lines 3 --width 40";
 
       monitor = ",preferred,auto,1";
 
       exec-once = [
-        "hyprpaper"
-        "zsh -c 'sleep 1 && pgrep .waybar || waybar'"
+        "${pkgs.hyprpaper}/bin/hyprpaper"
+        "${pkgs.waybar}/bin/waybar"
         "fcitx5 -d"
-        "dunst"
-        "nm-applet --indicator"
-        "zsh -c 'sleep 1 && blueman-applet &'"
+        "${pkgs.dunst}/bin/dunst"
+        "${pkgs.networkmanagerapplet}/bin/nm-applet --indicator"
+        "${pkgs.blueman}/bin/blueman-applet &"
         "mkdir -p $HOME/Pictures/Screenshots"
-        "wl-paste --watch cliphist store &"
-        "blueman-applet"
+        "${wlCopy} --watch ${cliphist} store &"
       ];
 
       # env variables
@@ -58,12 +90,12 @@
 
       # General
       general = {
-        gaps_in = 5;
-        gaps_out = 20;
-        border_size = 3;
-        "col.active_border" = "0xFF008000";
-        "col.inactive_border" = "0xFF202500";
-        resize_on_border = false;
+        gaps_in = 2;
+        gaps_out = 2;
+        border_size = 2;
+        "col.active_border" = "rgba(888888ff)";
+        "col.inactive_border" = "rgba(333333ff)";
+        resize_on_border = true;
         layout = "dwindle";
       };
 
@@ -107,11 +139,11 @@
       bind = [
         "$mainMod, RETURN, exec, $terminal"
         "$mainMod, Q, killactive,"
-        "$mainMod, T, exec, thunar"
+        "$mainMod, T, exec, ${fileManager}"
         "$mainMod, R, exec, $menu"
-        "$mainMod, L, exec, swaylock -f --screenshots --clock --effect-blur 7x5 --indicator --fade-in 0.5 --font 'Noto Sans CJK JP'"
-        "$mainMod, E, exec, wlogout"
-        "$mainMod, V, exec, cliphist list | fuzzel -d -w 80% | cliphist decode | wl-copy"
+        "$mainMod, L, exec, ${lock} -f --screenshots --clock --effect-blur 7x5 --indicator --fade-in 0.5 --font 'Noto Sans CJK JP'"
+        "$mainMod, E, exec, ${logout}"
+        "$mainMod, V, exec, ${cliphist} list | ${menu} -d -w 80% | ${cliphist} decode | ${wlCopy}"
         "$mainMod, M, exit,"
 
         "$mainMod, left, movefocus, l"
@@ -147,9 +179,9 @@
         "$mainMod SHIFT, 0, movetoworkspace, 10"
 
         # screenshot
-        ", Print, exec, bash -c 'FILEPATH=$(zenity --file-selection --save --confirm-overwrite --filename=\"$HOME/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S_active.png)\"); [ -n \"$FILEPATH\" ] && grim -g \"$(hyprctl activewindow -j | jq -r \"\\(.at[0]),\\(.at[1]) \\(.size[0])x\\(.size[1])\")\" \"$FILEPATH\"'"
-        "SHIFT, Print, exec, bash -c 'FILEPATH=$(zenity --file-selection --save --confirm-overwrite --filename=\"$HOME/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S_all.png)\"); [ -n \"$FILEPATH\" ] && grim \"$FILEPATH\"'"
-        "$mainMod, Print, exec, grim -g \"$(slurp)\" - | wl-copy"
+        ", Print, exec, ${screenshotActive}"
+        "SHIFT, Print, exec, ${screenshotAll}"
+        "$mainMod, Print, exec, ${grim} -g \"$(${slurp})\" - | ${wlCopy}"
 
         # window change rule
         "$mainMod, F, fullscreen"
@@ -164,17 +196,17 @@
 
       # binde / bindl
       binde = [
-        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-        ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
-        ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+        ", XF86AudioRaiseVolume, exec, ${wpctl} set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, ${wpctl} set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86MonBrightnessUp, exec, ${brightnessctl} set 5%+"
+        ", XF86MonBrightnessDown, exec, ${brightnessctl} set 5%-"
       ];
 
       bindl = [
-        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-        ", switch:on:Lid Switch, exec, hyprctl dispatch dpms off"
-        ", switch:off:Lid Switch, exec, hyprctl dispatch dpms on"
+        ", XF86AudioMute, exec, ${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioMicMute, exec, ${wpctl} set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ", switch:on:Lid Switch, exec, ${pkgs.hyprland}/bin/hyprctl dispatch dpms off"
+        ", switch:off:Lid Switch, exec, ${pkgs.hyprland}/bin/hyprctl dispatch dpms on"
       ];
     };
   };
