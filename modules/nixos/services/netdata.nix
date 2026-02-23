b49c7a16076db8709dfd2ca53af1f50284e77925
gitogito-san <gitogitohub@gmail.com>
2026-02-24 07:23:53 +0900

    netdate

--- modules/nixos/services/netdata.nix
+++ modules/nixos/services/netdata.nix
@@ -1,6 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  services.netdata.enable = true;
+  services.netdata = {
+    enable = true;
+    package = pkgs.netdataCloud;
+
+    config = {
+      global = {
+        "bind to" = "*";
+      };
+      web = {
+        "allow connections from" = "localhost 100.*";
+        "allow dashboard from" = "localhost 100.*";
+      };
+    };
+  };
+
   networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 19999 ];
 }
