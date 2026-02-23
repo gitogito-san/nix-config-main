4fb2a342c1c8926fe406a1a054ec0f5fbe08ad6b
gitogito-san <gitogitohub@gmail.com>
2026-02-24 01:36:42 +0900

    flake add home-manager-stable

--- flake.nix
+++ flake.nix
@@ -3,10 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
     };
     agenix = {
       url = "github:ryantm/agenix";
@@ -30,8 +35,9 @@
     {
       self,
       nixpkgs,
-      nixpkgs-stable,
       home-manager,
+      nixpkgs-stable,
+      home-manager-stable,
       agenix,
       stylix,
       niri,
@@ -75,7 +81,7 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/trigkey/default.nix
-          home-manager.nixosModules.default
+          home-manager-stable.nixosModules.default
           agenix.nixosModules.default
           {
             home-manager = {
