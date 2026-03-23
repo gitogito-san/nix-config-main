b852e713733fb15806be722761ed377aa2e35358
ya <gitogitohub@gmail.com>
2026-03-24 06:49:08 +0900

    fix: un-need sentence delete

--- modules/home-manager/desktop/cursor.nix
+++ modules/home-manager/desktop/cursor.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 let
   hackedCustomCursor = pkgs.stdenv.mkDerivation {
@@ -6,18 +6,13 @@ let
     version = "1.0";
 
     src = pkgs.fetchzip {
-      # URLの末尾を dl=1 に変更し、直接ダウンロードを強制する
       url = "https://www.dropbox.com/scl/fi/au99ozxjuo74yot/Hacked-Black.tgz?rlkey=vsbtxjvqb6ubq9ml175tyso1x&dl=1";
-      extension = "tgz"; # ← これを追加して拡張子を強制認識させる
-      # 初回は意図的にダミーハッシュを指定してエラーを起こす
+      extension = "tgz";
       hash = "sha256-LJseClgoQ5C4x0z3CCTAVoh4NOb87rAnmF+1G0RYU7U=";
     };
 
     installPhase = ''
-      # Nixストア内にアイコンの標準パスを作成
       mkdir -p $out/share/icons/Hacked-Black
-
-      # 展開された中身をすべてコピー
       cp -r * $out/share/icons/Hacked-Black/
     '';
   };
