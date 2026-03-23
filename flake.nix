44434d05bcedf684fe068d3e95c1516037a61576
ya <gitogitohub@gmail.com>
2026-03-24 06:48:41 +0900

    fix: flake improve

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -43,90 +44,113 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-stable,
-      nixpkgs-2411,
-      home-manager,
-      home-manager-stable,
-      nixvim,
-      agenix,
-      stylix,
-      stylix-stable,
-      treefmt-nix,
-      playit,
-      ...
-    }@inputs:
-    {
-      formatter.x86_64-linux =
-        (treefmt-nix.lib.evalModule nixpkgs.legacyPackages.x86_64-linux {
-          projectRootFile = "flake.nix";
-          programs.nixfmt.enable = true;
-          settings.formatter.nixfmt.excludes = [
-            "*.age"
-            "*.jpg"
-            "*.png"
-            "flake.lock"
-          ];
-        }).config.build.wrapper;
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { self, inputs, ... }:
+      {
+        imports = [
+          inputs.treefmt-nix.flakeModule
+        ];
 
-      nixosConfigurations.lifebook = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          { nixpkgs.hostPlatform = "x86_64-linux"; }
-          ./hosts/lifebook
-          home-manager.nixosModules.default
-          agenix.nixosModules.default
-          stylix.nixosModules.stylix
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = { inherit inputs; };
-              users.ya = {
-                imports = [
-                  ./home/ya/lifebook.nix
-                  inputs.nixvim.homeModules.nixvim
-                  ./modules/home-manager/tui/nixvim.nix
-                ];
-              };
-            };
-          }
+        systems = [
+          "x86_64-linux"
         ];
-      };
-      nixosConfigurations.trigkey = nixpkgs-stable.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          { nixpkgs.hostPlatform = "x86_64-linux"; }
-          ./hosts/trigkey
-          home-manager-stable.nixosModules.default
-          agenix.nixosModules.default
-          stylix-stable.nixosModules.stylix
-          playit.nixosModules.default
-          ./modules/nixos/services/uptime-kuma.nix
-          ./modules/nixos/services/reading.nix
-          (
-            { pkgs, ... }:
-            {
-              nixpkgs.overlays = [
-                (final: prev: {
-                  navidrome = nixpkgs-2411.legacyPackages.x86_64-linux.navidrome;
-                })
+
+        perSystem =
+          {
+            config,
+            pkgs,
+            system,
+            ...
+          }:
+          {
+            treefmt.config = {
+              projectRootFile = "flake.nix";
+              programs.nixfmt.enable = true;
+              settings.formatter.nixfmt.excludes = [
+                "*.age"
+                "*.jpg"
+                "*.png"
+                "flake.lock"
               ];
-            }
-          )
-          ./modules/nixos/services/navidrome.nix
-          ./modules/nixos/services/samba.nix
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = { inherit inputs; };
-              users.ya = ./home/ya/trigkey.nix;
             };
-          }
-        ];
-      };
-    };
+            devShells.default = pkgs.mkShell {
+              packages = [
+                inputs.agenix.packages.${system}.default
+                pkgs.statix
+                pkgs.deadnix
+                pkgs.nixfmt
+              ];
+            };
+          };
+
+        flake = {
+          nixosConfigurations = {
+
+            # ---------- Lifebook Configuration ----------
+            lifebook = inputs.nixpkgs.lib.nixosSystem {
+              specialArgs = { inherit inputs; };
+              modules = [
+                { nixpkgs.hostPlatform = "x86_64-linux"; }
+                ./hosts/lifebook
+                inputs.home-manager.nixosModules.default
+                inputs.agenix.nixosModules.default
+                inputs.stylix.nixosModules.stylix
+                {
+                  home-manager = {
+                    useGlobalPkgs = true;
+                    useUserPackages = true;
+                    extraSpecialArgs = { inherit inputs; };
+                    users.ya = {
+                      imports = [
+                        ./home/ya/lifebook.nix
+                        inputs.nixvim.homeModules.nixvim
+                        ./modules/home-manager/tui/nixvim.nix
+                      ];
+                    };
+                  };
+                }
+              ];
+            };
+
+            # ---------- Trigkey Configuration ----------
+            trigkey = inputs.nixpkgs-stable.lib.nixosSystem {
+              specialArgs = { inherit inputs; };
+              modules = [
+                { nixpkgs.hostPlatform = "x86_64-linux"; }
+                ./hosts/trigkey
+                inputs.home-manager-stable.nixosModules.default
+                inputs.agenix.nixosModules.default
+                inputs.stylix-stable.nixosModules.stylix
+                inputs.playit.nixosModules.default
+                ./modules/nixos/services/uptime-kuma.nix
+                ./modules/nixos/services/reading.nix
+                (
+                  { pkgs, ... }:
+                  {
+                    nixpkgs.overlays = [
+                      (final: prev: {
+                        navidrome = inputs.nixpkgs-2411.legacyPackages.${prev.stdenv.hostPlatform.system}.navidrome;
+                      })
+                    ];
+                  }
+                )
+                ./modules/nixos/services/navidrome.nix
+                ./modules/nixos/services/samba.nix
+                {
+                  home-manager = {
+                    useGlobalPkgs = true;
+                    useUserPackages = true;
+                    extraSpecialArgs = { inherit inputs; };
+                    users.ya = ./home/ya/trigkey.nix;
+                  };
+                }
+              ];
+            };
+
+          };
+        };
+
+      }
+    );
 }
