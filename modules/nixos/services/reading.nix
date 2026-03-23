3ee4ccac574b82827d702fa90519dde1cbd6dfb5
ya <gitogitohub@gmail.com>
2026-03-24 06:49:55 +0900

    fix: make them hash code

--- modules/nixos/services/reading.nix
+++ modules/nixos/services/reading.nix
@@ -9,7 +9,7 @@
   virtualisation.oci-containers.containers = {
 
     stirling-pdf = {
-      image = "stirlingtools/stirling-pdf:latest-fat";
+      image = "stirlingtools/stirling-pdf@sha256:6e01e599115e6269ee49f4421fa7a91c9f7c27186b8480899df3091f75d96d8e";
       ports = [ "8083:8080" ];
       volumes = [
         "/srv/stirling-pdf/tessdata:/usr/share/tessdata"
@@ -23,7 +23,7 @@
     };
 
     kavita = {
-      image = "jvmilazz0/kavita:latest";
+      image = "jvmilazz0/kavita@sha256:1f2acae7466d022f037ea09f7989eb7c487f916b881174c7a6de33dbfa8acb39";
       ports = [ "50000:5000" ];
       volumes = [
         "/srv/nas/books:/manga"
