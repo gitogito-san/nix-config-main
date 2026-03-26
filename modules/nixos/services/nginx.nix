7da509cf8498573fc413731f2b7e53423a85f747
gitogito-san <gitogitohub@gmail.com>
2026-03-26 09:04:33 +0900

    feat: nginx backend

--- modules/nixos/services/nginx.nix
+++ modules/nixos/services/nginx.nix
@@ -1,10 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.nginx = {
     enable = true;
 
     virtualHosts."timetable" = {
+      default = true;
       listen = [
         {
           addr = "0.0.0.0";
@@ -13,12 +14,28 @@
       ];
 
       root = "/var/www/timetable";
+
+      locations."/api/" = {
+        proxyPass = "http://127.0.0.1:3000";
+      };
     };
   };
 
   systemd.tmpfiles.rules = [
     "d /var/www/timetable 0775 ya users -"
   ];
+
+  systemd.services.timetable-backend = {
+    description = "Timetable Node.js Backend API";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.nodejs}/bin/node /var/www/timetable/server.js";
+      Restart = "always";
+      User = "ya";
+      StateDirectory = "timetable-backend";
+    };
+  };
 
   networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 8080 ];
 }
