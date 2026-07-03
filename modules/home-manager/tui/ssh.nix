be9ab5b43bce874c6bcc0d8872aa90f56e2ba4bc
ya <gitogitohub@gmail.com>
2026-07-03 21:27:53 +0900

    fix: ssh config

--- modules/home-manager/tui/ssh.nix
+++ modules/home-manager/tui/ssh.nix
@@ -1,4 +1,44 @@
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  osConfig,
+  ...
+}:
+
+let
+  sshSettings = {
+    "*" = {
+      controlMaster = "auto";
+      controlPath = "~/.ssh/%C";
+      controlPersist = "10m";
+    };
+
+    ya = {
+      hostname = "100.90.113.106";
+      user = "ya";
+      identityFile = "~/.ssh/id_main_minipc";
+    };
+
+    "github.com" = {
+      user = "git";
+      identityFile = "~/.ssh/github_ssh";
+    };
+
+    sol = {
+      hostname = "sol.cc.uec.ac.jp";
+      user = "y2511246";
+      identityFile = "~/.ssh/uec_key";
+    };
+
+    ced = {
+      hostname = "orange17.ced.cei.uec.ac.jp";
+      user = "y2511246";
+      proxyJump = "sol";
+      identityFile = "~/.ssh/uec_key";
+    };
+  };
+in
 
 {
   home.packages = [
@@ -8,41 +48,11 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-
-    matchBlocks = {
-      "*" = {
-        serverAliveInterval = 60;
-        serverAliveCountMax = 5;
-        extraOptions = {
-          ControlMaster = "auto";
-          ControlPath = "~/.ssh/%C";
-          ControlPersist = "10m";
-        };
-      };
-
-      ya = {
-        hostname = "100.90.113.106";
-        user = "ya";
-        identityFile = "~/.ssh/id_main_minipc";
-      };
-
-      "github.com" = {
-        user = "git";
-        identityFile = "~/.ssh/github_ssh";
-      };
-
-      sol = {
-        hostname = "sol.cc.uec.ac.jp";
-        user = "y2511246";
-        identityFile = "~/.ssh/uec_key";
-      };
-
-      ced = {
-        hostname = "orange17.ced.cei.uec.ac.jp";
-        user = "y2511246";
-        proxyJump = "sol";
-        identityFile = "~/.ssh/uec_key";
-      };
-    };
-  };
+  }
+  // (
+    if (osConfig.networking.hostName == "trigkey") then
+      { matchBlocks = sshSettings; }
+    else
+      { settings = sshSettings; }
+  );
 }
